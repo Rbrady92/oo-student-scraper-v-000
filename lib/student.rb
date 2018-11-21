fe064f2f3805e5da83eda2a4a5c0487a21c9cecd
Ryan Brady <ryanbrady369@gmail.com>
2018-11-21 21:13:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,6 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each {|attribute, info| self.send("#{attribute}=", info)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
